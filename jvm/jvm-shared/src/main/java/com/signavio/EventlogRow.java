41d65f5baf1b5f7b9e9ed38135ba1991c22755f7
Evgenii Lartcev <i.dolende@gmail.com>
2021-11-15 03:35:50 +0300

    equals and hashcode to avoid duplication

--- jvm/jvm-shared/src/main/java/com/signavio/EventlogRow.java
+++ jvm/jvm-shared/src/main/java/com/signavio/EventlogRow.java
@@ -1,16 +1,30 @@
 package com.signavio;
 
 import java.time.OffsetDateTime;
+import java.util.Objects;
 
 public class EventlogRow {
-	
+
 	String caseId;
 	String eventName;
 	OffsetDateTime timestamp;
-	
+
 	public EventlogRow(String caseId, String eventName, OffsetDateTime timestamp) {
 		this.caseId = caseId;
 		this.eventName = eventName;
 		this.timestamp = timestamp;
 	}
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final EventlogRow that = (EventlogRow) o;
+        return Objects.equals(caseId, that.caseId) && Objects.equals(eventName, that.eventName) && Objects.equals(timestamp, that.timestamp);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(caseId, eventName, timestamp);
+    }
 }
